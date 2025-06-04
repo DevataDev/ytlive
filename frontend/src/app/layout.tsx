6430f519a7485df98cc471ba5a805ea2a7052f1e
Devata <devatadev@pm.me>
2025-06-05 01:47:18 +0800

    media file selection

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -48,6 +48,16 @@ export default function RootLayout({
     require('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, []);
 
+  // preload config
+  useEffect(() => {
+    const preloadConfig = async () => {
+      const res = await fetch('/api/config');
+      const data = await res.json();
+      console.log('preload config', data);
+    };
+    preloadConfig();
+  })
+
   const shouldHide = hideHeaderFooter.includes(pathname);
 
   return (
